c8a4a94f5e6d5f5ee943129837b99c69adc19882
Pieterjan Montens <pmo@upulvan.raadvst-consetat.be>
2017-07-26 14:34:07 +0200

    More groundwork

--- common/the_parser.adb
+++ common/the_parser.adb
@@ -6,6 +6,8 @@ package body the_parser is
     function parse_request(Data:Ada.Streams.Stream_Element_Array) return Request_Record is
     begin
         -- Just some defaults, awaiting implementation
+        -- Probably some kind of byte-for-byte interface & state machine.
+
         Request := (method  => Get
                    ,path    => parse_path(Data)
                    ,version => http11);
@@ -15,6 +17,8 @@ package body the_parser is
 
     function parse_path(Data:Ada.Streams.Stream_Element_Array) return Request_Path is
     begin
+        -- Just some defaults, awaiting implementation
+        -- Probably read allowed characters until whitespace
         return Ada.Strings.Unbounded.To_Unbounded_String("/");
     end parse_path;
 
